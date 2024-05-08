0c3e7e8d39b2201135655a21b8b002ba89b538cf
Dipu67 <mddipu2482@gmail.com>
2024-05-08 15:08:59 +0600

    first commit

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.set('view engine','ejs')
 app.get('/',(req,res) =>{
     res.render('home')
 })
+app.get('/profile',(req,res) =>{
+    res.render('profile')
+})
+app.get('/home',(req,res) =>{
+    res.render('home')
+})
 
 app.listen(3000,()=>{
     console.log('working server');
