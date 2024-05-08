69541a507b8e5a6410a3b33eef9da102bfc04ee3
Dipu67 <mddipu2482@gmail.com>
2024-05-08 18:16:48 +0600

    first commit

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const port = process.env.PORT;
+// const userModel = require('./utils/user')
 
-const app = express()
+const app = express();
 
-app.set('view engine','ejs')
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get('/',(req,res) =>{
-    res.render('home')
-})
-app.get('/profile',(req,res) =>{
-    res.render('profile')
-})
-app.get('/home',(req,res) =>{
-    res.render('home')
-})
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/profile", (req, res) => {
+  res.render("profile");
+});
+app.get("/create", async (req, res) => {
+  const userSchema = mongoose.Schema({
+    name: String,
+    email: String,
+    password: String,
+  });
+  let userModel = mongoose.model("user", userSchema);
 
-app.listen(3000,()=>{
-    console.log('working server');
-})
+  let user = await userModel.create({
+    name: "dipu",
+    email: "dipu@gmail.com",
+    password: "dipu",
+  });
+
+  res.send("working");
+});
+
+app.listen(process.env.PORT, (err) => {
+  console.log("working server ");
+});
--- utils/user.js
+++ utils/user.js
@@ -1,2 +1,12 @@
 const mongoose = require('mongoose')
 
+// mongoose.connect('mongodb+srv://mddipu:Dipu@?12@a24.ypywfvd.mongodb.net/')
+mongoose.connect("mongodb://127.0.1:27017/a24")
+
+const userSchema = mongoose.Schema({
+    name:String,
+    email: String,
+    password: String
+})
+
+module.exports = mongoose.model('user',userSchema);
